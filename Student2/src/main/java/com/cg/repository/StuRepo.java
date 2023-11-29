72a88d8793c66b738b48da1c2d9e2d8fefe86162
CheekatlaSnigdha <119494906+CheekatlaSnigdha@users.noreply.github.com>
2023-11-29 10:21:33 +0530

    Delete Student2 directory

--- Student2/src/main/java/com/cg/repository/StuRepo.java
+++
@@ -1,9 +0,0 @@
-package com.cg.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.cg.model.StudentInfo;
-
-public interface StuRepo extends MongoRepository<StudentInfo, Long> {
-
-}
